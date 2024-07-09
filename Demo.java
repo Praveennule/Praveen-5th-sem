55c348cc4f6e5d216bf806795c213779fa802e7a
Praveennule <>
2024-07-09 11:26:25 +0545

    .

---
+++ Demo.java
@@ -0,0 +1,19 @@
+//program to demonstarte if condition
+import java.util.Scanner;
+public class Demo.java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter sem");
+		int sem=sc.nextInt();
+		
+		if(sem==4)
+		{
+			System.out.println("true block sem4");
+		}
+		System.out.println("Next statement");
+	}
+
+}
